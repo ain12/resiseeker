8128f9f8f588a651376259cff977236ccbb07a60
ainhoa <ainhoaactic@gmail.com>
2023-06-03 17:06:11 +0200

    log in functionality

---
+++ app/components/Modals/LoginModal.tsx
@@ -0,0 +1,88 @@
+'use client';
+import axios from 'axios';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle} from 'react-icons/fc';
+import { useCallback, useEffect, useState } from "react";
+import { SubmitHandler, useForm, FieldValues } from "react-hook-form";
+import Modal from './Modal';
+import Heading from '../Heading';
+import Input from '../Inputs/Input';
+import {toast} from 'react-hot-toast';
+import Button from '../Button';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+
+
+const LoginModal = () => {
+    const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> =  (data) => {
+        setIsLoading(true);
+
+        signIn('credentials', {
+            ...data,
+            redirect: false,
+        }).then((cb) => {
+            setIsLoading(false);
+            if(cb?.ok) {
+                toast.success('Logged in successfully!');
+                router.refresh();
+                loginModal.onClose();
+            }
+            if(cb?.error) {
+                toast.error(cb.error);
+            }
+        })
+    };
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <Heading title="Welcome back" description="Log in to your account!"/>
+            <Input id="email" label="Email" register={register} errors={errors} disabled={isLoading} required/>
+            <Input id="password" type="password" label="Password" register={register} errors={errors} disabled={isLoading} required/>
+        </div>
+    );
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button label="Continue with Google" icon={FcGoogle} onClick={() => {}} outline/>
+            <Button label="Continue with Github" icon={AiFillGithub} onClick={() => {}} outline/>
+            <div className="text-center mt-4 text-neutral-500 font-light">
+                <div className="flex flex-row items-center justify-center gap-2 text-center">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div onClick={registerModal.onClose} className="cursor-pointer hover:underline text-neutral-800">
+                        Log in
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+    return (
+        <Modal disabled={isLoading} isOpen={loginModal.isOpen} 
+        actionLabel="Continue" title="Log in" body={bodyContent} footer={footerContent}
+        onSubmit={handleSubmit(onSubmit)} onClose={loginModal.onClose}/>
+    );
+};
+
+export default LoginModal;
--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -4,10 +4,20 @@ import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useLoginModal from "@/app/hooks/useLoginModal";
+import { User } from "@prisma/client";
+import { signOut } from "next-auth/react";
 
-const UserMenu = () => {
+interface UserMenuProps {
+    currentUser?: User | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+    currentUser
+}) => {
     const [isOpen, setIsOpen] = useState(false);
     const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
 
     const toggleMenu = useCallback(() => {
         setIsOpen((prev) => !prev);
@@ -30,8 +40,22 @@ const UserMenu = () => {
             {isOpen && (
                 <div className="rounded-xl absolute right-0 text-sm top-12 shadow-md w-[40vw ] md:w-3/4 bg-white overflow-hidden">
                     <div className="flex flex-col cursor-pointer">
-                        <MenuItem onClick={() => {}} label="Log in"/>
+                        {currentUser ? (
+                            <>
+                            <MenuItem onClick={() => {}} label="My trips"/>
+                            <MenuItem onClick={() => {}} label="My favorites"/>
+                            <MenuItem onClick={() => {}} label="My reservations"/>
+                            <MenuItem onClick={() => {}} label="My properties"/>
+                            <MenuItem onClick={() => {}} label="ResiSeeker my home"/>
+                            <hr />
+                            <MenuItem onClick={() => signOut()} label="Log out"/>
+                            </>
+                           ): (
+                            <>
+                        <MenuItem onClick={loginModal.onOpen} label="Log in"/>
                         <MenuItem onClick={registerModal.onOpen} label="Sign up"/>
+                        </>
+                           ) }
                     </div>
                 </div>
             )}
