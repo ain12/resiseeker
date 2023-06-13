5f232c93599deb30d071a5123ad68862024911df
ainhoa <ainhoaactic@gmail.com>
2023-06-13 13:00:31 +0200

    location page modal and world map

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -6,6 +6,8 @@ import Heading from "../Heading";
 import { categories } from "../Navbar/Categories";
 import CategoryInput from "../Inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../Inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -43,12 +45,16 @@ const RentModal = () => {
         }
     });
 
-    //const location = watch('location');
+    const location = watch('location');
     const category = watch('category');
     /* const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
     const imageSrc = watch('imageSrc'); */
+
+    const Map = useMemo(() => dynamic(() => import('../Map'), {
+        ssr: false,
+    }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -87,6 +93,16 @@ const RentModal = () => {
         </div>
     );
 
+    if(step === STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading title="Where is your place located?" description="Help guests find you!"/>
+                <CountrySelect value={location} onChange={(value) => setCustomValue('location', value)}/>
+                <Map center={location?.latlng}/>
+            </div>
+        )
+    }
+
     const secondaryActionLabel = useMemo(() => {
         if (step === STEPS.CATEGORY) {
             return undefined;
@@ -96,7 +112,7 @@ const RentModal = () => {
     }, [step]);
 
     return(
-        <Modal title="Your ResiSeeker home" isOpen={rentModal.isOpen} onSubmit={rentModal.onClose}
+        <Modal title="Your ResiSeeker home" isOpen={rentModal.isOpen} onSubmit={onNext}
         onClose={rentModal.onClose} actionLabel={actionLabel} secondaryActionLabel={secondaryActionLabel} 
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} body={bodyContent}/>
     )
