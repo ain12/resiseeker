0993c12a1cd721179ee922458d1f150722ce845b
ainhoa <ainhoaactic@gmail.com>
2023-07-01 13:03:36 +0200

    initial work for listening properties

---
+++ app/components/Listings/ListingCard.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import {Listing, Reservation} from "@prisma/client";
+import { on } from "events";
+import { useRouter } from "next/navigation";
+import { use, useCallback, useMemo } from "react";
+import {format} from 'date-fns';
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
+
+interface ListingCardProps {
+    data: Listing;
+    reservation?: Reservation;
+    onAction?: (id: string) => void;
+    disabled?: boolean;
+    actionLabel?: string;
+    actionId?: string;
+    currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+    data,
+    reservation,
+    onAction,
+    disabled,
+    actionLabel,
+    actionId = "",
+    currentUser
+}) => {
+    const router = useRouter();
+    const { getByValue } = useCountries();
+
+    const location = getByValue(data.locationValue);
+
+    const handleCancel = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+            if(disabled) {
+                return;
+            }
+
+            onAction?.(actionId);
+        }, [onAction, actionId, disabled]
+    )
+
+    const price = useMemo(() => {
+        if(reservation) {
+            return reservation.totalPrice;
+        }
+
+        return data.price;
+    }, [reservation, data.price]);
+
+    const reservationDate = useMemo(() => {
+
+        if(!reservation) {
+            return null;
+        }
+
+        const startDate = new Date(reservation.startDate);
+        const endDate = new Date(reservation.endDate);
+
+        return `${format(startDate, 'PP')} - ${format(endDate, 'PP')}`;
+    }, [reservation]);
+
+    return (
+        <div className="col-span-1 cursor-pointer group" onClick={() => router.push(`/listings/${data.id}`)}>
+            <div className="flex flex-col w-full gap-2">
+                <div className="relative w-full aspect-square rounded-xl overflow-hidden">
+                    <Image src={data.imageSrc} alt={"listing"} className="h-full w-full object-cover group-hover:scale-110 transition" fill/>
+                    <div className="absolute right-3 top-3">
+                        <HeartButton listingId={data.id} currentUser={currentUser}/>
+                    </div>
+                </div>
+                <div className="text-lg font-semibold">
+                    {location?.region}, {location?.label}
+                </div>
+                <div className="font-light text-neutral-500">
+                    {reservationDate || data.category} 
+                </div>
+                <div className="flex flex-row items-center gap-1">
+                    <div className="font-semibold">
+                         {price} €
+                    </div>
+                    {!reservation && (
+                        <div className="text-light">
+                            /night
+                        </div>
+                        )}
+                </div>
+                {onAction && actionLabel && (
+                    <Button disabled={disabled} small label={actionLabel} onClick={handleCancel}/>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default ListingCard;
