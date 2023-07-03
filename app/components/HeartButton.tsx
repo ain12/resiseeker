72ba0967edec8aa33ba4ccf22fc51a959a437028
ainhoa <ainhoaactic@gmail.com>
2023-07-03 11:43:13 +0200

    "client component" warning solved

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -24,7 +24,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
 
   return (
     <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
-      <AiOutlineHeart size={28} className="fill-white absolute top-[2px] -right-[2px]"/>
+      <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]"/>
       <AiFillHeart size={24} className={
           hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
         }
